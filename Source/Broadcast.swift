e1d39da69591fe1238f3b28d852c1b5ebcc41fc1
Benedikt Veith <bv@scherer-software.de>
2018-05-12 13:10:42 +0200

    Broadcast - Added Documentation

--- Source/Broadcast.swift
+++ Source/Broadcast.swift
@@ -32,6 +32,12 @@ struct JSONStringify {
     
     let value: Any
     
+    
+    /// Converts NSDictionary to valid JSON string
+    ///
+    /// - Parameter prettyPrinted: if json string should be pretty printed
+    /// - Returns: JSON String
+    /// - Throws: StringifyError, if not valid JSON Object
     func stringify(prettyPrinted: Bool = false) throws -> String {
         let options: JSONSerialization.WritingOptions = prettyPrinted ? .prettyPrinted : .init(rawValue: 0)
         if JSONSerialization.isValidJSONObject(self.value) {
@@ -60,6 +66,15 @@ public class Broadcast {
     
     public init() {}
     
+    
+    /// Vote for a post
+    ///
+    /// - Parameters:
+    ///   - voter: Voter account name
+    ///   - author: Author account name
+    ///   - permlink: Post Permlin
+    ///   - weight: Vote weight (max 10000)
+    ///   - completion: Returns JSON Object
     public func vote(voter: String, author: String, permlink: String, weight: Int, completion:((JSONString) -> Void)?) {
         Steem.sharedInstance.auth.call(url: "/broadcast", data: [
             "operations": [
@@ -74,6 +89,14 @@ public class Broadcast {
         ], callback: completion!)
     }
     
+    
+    /// Reblog a post
+    ///
+    /// - Parameters:
+    ///   - account: Reblogger account name
+    ///   - author: Author account name
+    ///   - permlink: Post permlin
+    ///   - completion: Returns JSON Object
     public func reblog(account: String, author: String, permlink: String, completion:((JSONString) -> Void)?) {
         do {
             let json = try JSONStringify(value: [
@@ -102,6 +125,13 @@ public class Broadcast {
         }
     }
     
+    
+    /// Post custom json inside account transaction history
+    ///
+    /// - Parameters:
+    ///   - account: account name
+    ///   - json: custom json data
+    ///   - completion: Returns JSON Object
     public func customJSON(account: String, json: JSONString, completion:((JSONString) -> Void)?) {
         do {
             let json = try JSONStringify(value: json).stringify()
@@ -124,6 +154,13 @@ public class Broadcast {
     
     // PRAGMA MARK: FOLLOW API
     
+    
+    /// Follow an account
+    ///
+    /// - Parameters:
+    ///   - follower: Follower account name
+    ///   - following: Following account name
+    ///   - completion: Returns JSON Object
     public func follow(follower: String, following: String, completion:((JSONString) -> Void)?) {
         do {
             let json = try JSONStringify(value: [
@@ -151,6 +188,13 @@ public class Broadcast {
         }
     }
     
+    
+    /// Unfollow an account
+    ///
+    /// - Parameters:
+    ///   - unfollower: Unfollower account name
+    ///   - unfollowing: Unfollowing account name
+    ///   - completion: Returns JSON Object
     public func unfollow(unfollower: String, unfollowing: String, completion:((JSONString) -> Void)?) {
         do {
             let json = try JSONStringify(value: [
@@ -178,6 +222,13 @@ public class Broadcast {
         }
     }
     
+    
+    /// Ignore an account
+    ///
+    /// - Parameters:
+    ///   - follower: Ignorer account name
+    ///   - following: Ignoring account name
+    ///   - completion: Returns JSON Object
     public func ignore(follower: String, following: String, completion:((JSONString) -> Void)?) {
         do {
             let json = try JSONStringify(value: [
@@ -207,6 +258,15 @@ public class Broadcast {
     
     // PRAGMA MARK: CLAIM
     
+    
+    /// Claim Account Rewards Balance
+    ///
+    /// - Parameters:
+    ///   - account: account name
+    ///   - rewardSBD: SBD Amount to claim
+    ///   - rewardSteem: Steem Amount to claim
+    ///   - rewardVests: Vests (SP) Amount to claim
+    ///   - completion: Returns JSON Object
     public func claimRewardBalance(account: String, rewardSBD: String, rewardSteem: String, rewardVests: Int, completion:((JSONString) -> Void)?) {
         Steem.sharedInstance.auth.call(url: "/broadcast", data: [
             "operations": [
@@ -223,6 +283,18 @@ public class Broadcast {
     
     // PRAGMA MARK: COMMENT
     
+    
+    /// Create a post or comment
+    ///
+    /// - Parameters:
+    ///   - parentAuthor: If commenting else empty
+    ///   - parentPermlink: If commenting else empty
+    ///   - author: Author account name
+    ///   - permlink: Post Permlink
+    ///   - title: Post Title
+    ///   - body: Post Content
+    ///   - jsonMetadata: Post JSON Metada; see JSONMetadata for possible values
+    ///   - completion: Returns JSON Object
     public func comment(parentAuthor: String?, parentPermlink: String?, author: String, permlink: String, title: String, body: String, jsonMetadata: JSONMetadata, completion:((JSONString) -> Void)?) {
         
         do {
@@ -255,6 +327,18 @@ public class Broadcast {
         }
     }
     
+    
+    /// Set Comment Options to an Post
+    ///
+    /// - Parameters:
+    ///   - author: Author account name
+    ///   - permlink: Post Permlink
+    ///   - maxAcceptedPayout: Max Accepted Payout
+    ///   - percentSteemDollars: Should be 10000
+    ///   - allowVotes: Allow Votes on Post
+    ///   - allowCurationRewards: Allow Curation Rewards on Post
+    ///   - extensions: Extensions (e.g beneficiaries)
+    ///   - completion: Returns JSON Object
     public func commentOptions(author : String, permlink: String, maxAcceptedPayout: String?, percentSteemDollars: Int?, allowVotes : Bool, allowCurationRewards : Bool, extensions: NSDictionary?, completion:((JSONString) -> Void)?) {
         
         Steem.sharedInstance.auth.call(url: "/broadcast", data: [
@@ -272,6 +356,13 @@ public class Broadcast {
         ], callback: completion!)
     }
     
+    
+    /// Deletes a Post
+    ///
+    /// - Parameters:
+    ///   - author: Author account name
+    ///   - permlink: Post Permlink
+    ///   - completion: Returns JSON Object
     public func commentDelete(author : String, permlink: String, completion:((JSONString) -> Void)?) {
         Steem.sharedInstance.auth.call(url: "/broadcast", data: [
             "operations": [
